b441e240f90f08992a68514858cc3b95e705f96a
szakorz <longyutao310@gmail.com>
2025-09-02 09:14:01 +0800

    Update auto_sign2.py

--- auto_sign2.py
+++ auto_sign2.py
@@ -3,8 +3,8 @@ import time
 
 class ChaoxingAutoSign:
     def __init__(self):
-        self.username = "19375185810"
-        self.password = "Yanshaoyun111"
+        self.username = "15399756985"
+        self.password = "chenyu123"
         self.session = requests.Session()
         self.session.headers.update({
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
@@ -92,7 +92,7 @@ class ChaoxingAutoSign:
 
     def run(self):
         self.login()
-        self.wait_until(target_time="09:40:00")
+        self.wait_until(target_time="08:40:00")
         time.sleep(2)
         reserves = self.get_reserve_list()
         if not reserves:
